02b56992de04a6436ed8772995bdb91641cbcbf0
Matthew Russo <matthew@matthewclayrusso.com>
2020-02-14 22:41:58 -0800

    adds blog routing

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,70 @@ use std::io::prelude::*;
 use regex::Regex;
 use std::error::Error;
 
+const RESOURCES: &str = "/Users/matthewrusso/projects/rust/matthewclayrusso_com/resources";
+const BLOG: &str = "/Users/matthewrusso/projects/rust/matthewclayrusso_com/blog";
+
 fn main() {
-    let addr = "0.0.0.0:7777".parse().unwrap();
+    let addr = "0.0.0.0:4919".parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(Server)).unwrap();
     server.run().unwrap();
 }
 
 struct Server;
+
+impl Server {
+    fn serve_blog(&self, req: &Request) -> Option<Vec<u8>> {
+        let blog_re = Regex::new(r"^/blog(?:/([a-zA-Z_-]+))?$").unwrap();
+
+        if let Some(caps) = blog_re.captures(req.path()) {
+            let filename = if caps.len() == 2 {
+                if let Some(c) = caps.get(1) {
+                    c.as_str()
+                } else {
+                    "index"
+                }
+            } else {
+                "index"
+            };
+
+            return File::open(format!("{}/{}.html", BLOG, filename))
+                .ok()
+                .map(|mut file| {
+                    let mut contents: Vec<u8> = Vec::new();
+                    file.read_to_end(&mut contents).expect("Something went wrong opening the file");
+                    contents
+                });
+        } else {
+            None
+        }
+    }
+
+    fn serve_static(&self, req: &Request) -> Option<Vec<u8>> {
+        let static_re = Regex::new(r"^/static/((?:[a-zA-Z_-]+/)+[a-zA-Z_-]+\.[a-zA-Z]+)$").unwrap();
+
+        if let Some(caps) = static_re.captures(req.path()) {
+            let filename = caps.get(1).unwrap().as_str();
+
+            return File::open(format!("{}/{}", RESOURCES, filename))
+                .ok()
+                .map(|mut file| {
+                    let mut contents: Vec<u8> = Vec::new();
+                    file.read_to_end(&mut contents).expect("Something went wrong opening the file");
+                    contents
+                });
+        } else {
+            None
+        }
+    }
+
+    fn serve_index(&self) -> Vec<u8> {
+        let html = "/Users/matthewrusso/projects/rust/matthewclayrusso_com/resources/html/index.html";
+        let mut f = File::open(html).expect("file not found");
+        let mut contents: Vec<u8> = Vec::new();
+        f.read_to_end(&mut contents).expect("something went wrong reading the file");
+        contents
+    }
+}
 
 impl Service for Server {
     type Request = Request;
@@ -27,39 +84,13 @@ impl Service for Server {
     type Future = Box<dyn Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
-        let resources = "/home/ec2-user/matthewclayrusso_com/resources/";
-        let html = "/home/ec2-user/matthewclayrusso_com/resources/html/index.html";
-        let mut contents: Vec<u8> = Vec::new();
-
-        let re = Regex::new(r"^/static/((?:[a-zA-Z_-]+/)+[a-zA-Z_-]+\.[a-zA-Z]+)$").unwrap();
-
-        match re.captures(req.path()) {
-            Some(caps) => {
-                if caps.len() == 2 {
-                    let filename = caps.get(1).unwrap().as_str();
-
-                    let composed = format!("{}{}", resources, filename);
-                    println!("{}", composed);
-
-                    match File::open(composed) {
-                        Ok(mut file) => {
-                            file.read_to_end(&mut contents).expect("Something went wrong opening the file");
-                        },
-                        Err(e) => {
-                            println!("ERROR READING FILE: {}", e.description());
-                        },
-                    }
-                } else {
-                    println!("not enough captures: {}", caps.len());
-                }
-            },
-            None => {
-                let mut f = File::open(html).expect("file not found");
-
-                f.read_to_end(&mut contents)
-                    .expect("something went wrong reading the file");
-            }
-        }
+        let contents = if let Some(contents) = self.serve_blog(&req) {
+            contents
+        } else if let Some(contents) = self.serve_static(&req) {
+            contents
+        } else {
+            self.serve_index()
+        };
 
         Box::new(futures::future::ok(
             Response::new()
